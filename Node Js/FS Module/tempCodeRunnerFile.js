0d3ead1350f70b674a9c9ccbdf1448f9e18a9a2d
Rohit551800 <rohitsingh139915@gmail.com>
2026-04-02 19:30:48 +0530

    File structure changes

--- Node Js/FS Module/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-const readingFile = fs.readFile(filePath , "utf-8" , (err , data) => {
-    if(err) console.error(err);
-    else console.log(data);
-})
---
+++ Node Js/MODULES/tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+
+// const readFile = fs.writeFile(filePath , "Hii i am currently writing the file" , "utf-8")
+//     .then(() => console.log("File write successfully"))
+//     .catch((err) => console.log(err));
+
+
+// const appendFile = fs.writeFile(filePath , "Hii i am currently writing the file" , "utf-8")
+//     .then(() => console.log("File write successfully"))
+//     .catch((err) => console.log(err));
+
+
+// const deleteFile = fs.writeFile(filePath , "Hii i am currently writing the file" , "utf-8")
+//     .then(() => console.log("File write successfully"))
+//     .catch((err) => console.log(err));
